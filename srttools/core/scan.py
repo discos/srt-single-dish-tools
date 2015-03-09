f008660477f0b274fa1d80e08bd648395fee4e19
mbachett <matteo.bachetti@irap.omp.eu>
2015-03-09 12:03:32 +0100

    Loads an entire set of scans; (+ function to test it)

--- srttools/core/scan.py
+++ srttools/core/scan.py
@@ -1,22 +1,42 @@
 from .io import read_data
 import glob
 from .read_config import read_config
+import os
 
 
 class Scan():
     '''Class containing a single scan'''
     def __init__(self, fname):
+        self.filename = fname
         self.table = read_data(fname)
         for col in self.table.columns:
             setattr(self, col, self.table.field(col))
 
-    def baseline_subtract():
+    def baseline_subtract(self):
         pass
+
+    def __repr__(self):
+        reprstring = '\n\n----Scan from file {} ----\n'.format(self.filename)
+        reprstring += repr(self.table)
+        return reprstring
 
 
 class ScanSet():
-    def __init__(self):
-        config = read_config
+    def __init__(self, config_file=None):
+        self.config = read_config(config_file)
+        self.scans = []
+        datadir = self.config['datadir']
+        dirlist = self.config['list_of_directories']
+        for d in dirlist:
+            for f in glob.glob(os.path.join(datadir, d, '*')):
+                self.scans.append(Scan(f))
+
+    def __repr__(self):
+        reprstring = ''
+        for s in self.scans:
+            reprstring += repr(s)
+
+        return reprstring
 
 
 def test_scan():
@@ -36,3 +56,13 @@ def test_scan():
     plt.show()
 
 
+def test_scanset():
+    '''Test that sets of data are read.'''
+    import os
+    curdir = os.path.abspath(os.path.dirname(__file__))
+    config = os.path.join(curdir, '..', '..', 'TEST_DATASET',
+                          'test_config.ini')
+
+    scanset = ScanSet(config)
+
+    print(scanset)
