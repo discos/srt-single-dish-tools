acb23dc66c90bd93ea25e87146213354b52382a2
mbachett <matteo.bachetti@irap.omp.eu>
2015-03-09 11:37:09 +0100

    Implements default values in config, including default directory list (if config file has empty directory list, use all directories in datadir)

--- srttools/core/read_config.py
+++ srttools/core/read_config.py
@@ -1,8 +1,11 @@
+import os
+import glob
 # For Python 2 and 3 compatibility
 try:
     import configparser
 except ImportError:
     import ConfigParser as configparser
+
 
 def read_config(fname):
     '''Read a config file and return a dictionary of all entries'''
@@ -11,25 +14,43 @@ def read_config(fname):
 
     Config = configparser.ConfigParser()
 
-    dum = Config.read(fname)
+    Config.read(fname)
+
+    # ---------- Set default values --------------------------------------
+
+    config_output['projection'] = 'ARC'
+    config_output['interpolation'] = 'linear'
+    config_output['workdir'] = './'
+    config_output['datadir'] = os.path.abspath(
+        os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                     '..', '..', 'TEST_DATASET'))
+    config_output['list_of_directories'] = '*'
+
+    # --------------------------------------------------------------------
 
     # Read local information
 
     local_params = dict(Config.items('local'))
-    print(local_params)
-    config_output['workdir'] = local_params['work_directory']
-    config_output['datadir'] = local_params['data_directory']
+
+    config_output.update(local_params)
 
     # Read analysis information
     analysis_params = dict(Config.items('analysis'))
+
+    config_output.update(analysis_params)
+
     config_output['list_of_directories'] = \
         [s for s in analysis_params['list_of_directories'].splitlines()
          if s.strip()]  # This last instruction eliminates blank lines
 
-    config_output['projection'] = analysis_params['projection']
-    config_output['interpolation'] = analysis_params['interpolation']
-    config_output['prefix'] = analysis_params['prefix']
+    # If the list of directories is not specified, or if a '*' symbol is used,
+    # use glob in the datadir to determine the list
 
+    if config_output['list_of_directories'] in ([], ['*'], '*'):
+        config_output['list_of_directories'] = \
+            [os.path.split(f)[1]  # return name without path
+             for f in glob.glob(os.path.join(config_output['datadir'], '*'))
+             if os.path.isdir(f)]  # only if it's a directory
     return config_output
 
 
@@ -43,5 +64,21 @@ def test_read_config():
 
     config = read_config(fname)
 
+    print('\n --- Test config file ---')
+    for k in config.keys():
+        print("{}: {}".format(k, config[k]))
+
+
+def test_read_incomplete_config():
+    '''Test that config file are read.'''
+    import os
+    curdir = os.path.abspath(os.path.dirname(__file__))
+    datadir = os.path.join(curdir, '..', '..', 'TEST_DATASET')
+
+    fname = os.path.join(datadir, 'test_config_incomplete.ini')
+
+    config = read_config(fname)
+
+    print('\n --- Test incomplete config file ---')
     for k in config.keys():
         print("{}: {}".format(k, config[k]))
