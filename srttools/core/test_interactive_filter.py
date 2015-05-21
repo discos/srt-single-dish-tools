7c5305dbbbbc04fb0f9834591bb7ca2b0152255f
mbachett <matteo.bachetti@irap.omp.eu>
2015-05-21 16:53:32 +0200

    Tests moved to new files

---
+++ srttools/core/test_interactive_filter.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, unicode_literals, division,
+                        print_function)
+
+from matplotlib import pyplot as plt
+import numpy as np
+from .interactive_filter import select_data, ImageSelector
+
+
+def test_select_data():
+    times = np.arange(0, 100, 0.1)
+    lc1 = np.random.normal(0, 0.3, len(times)) + times * 0.5 +\
+        10 * np.exp(-(times - 30) ** 2 / 20 ** 2) + \
+        3 * np.exp(-(times - 55) ** 2 / 6 ** 2) + \
+        10 * np.exp(-(times - 80) ** 2 / 0.1 ** 2)
+    lc2 = np.random.normal(0, 0.3, len(times)) + times * 0.3 +\
+        10 * np.exp(-(times - 30) ** 2 / 20 ** 2) + \
+        3 * np.exp(-(times - 55) ** 2 / 6 ** 2) + \
+        10 * np.exp(-(times - 80) ** 2 / 0.1 ** 2)
+    xs = {'Ch0': times, 'Ch1': times}
+    ys = {'Ch0': lc1, 'Ch1': lc2}
+    select_data(xs, ys)
+
+
+def test_imageselector():
+    img = np.random.normal(100, 100, (100, 100))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    imagesel = ImageSelector(img, ax)
+
+    plt.show()
