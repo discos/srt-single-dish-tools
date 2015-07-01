ff4b48b20f5c639f448ee770f8b00cdecd9b8b67
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-02 01:26:08 +0200

    Better package import

--- srttools/core/__init__.py
+++ srttools/core/__init__.py
@@ -2,4 +2,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 from . import io
 from . import read_config
-
+from . import calibration
+from . import scan
+from . import fit
+from . import interactive_filter
