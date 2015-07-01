d7bf6cc1beaf2269619756dcaaad5f7c54bef2fd
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-02 01:26:29 +0200

    Some refactoring

--- srttools/core/calibration.py
+++ srttools/core/calibration.py
@@ -7,107 +7,194 @@ from .scan import Scan, list_scans
 from .read_config import read_config
 from .fit import fit_baseline_plus_bell
 import os
-import glob
+
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy.table import Table, Column
+from astropy.table import Table, vstack
 
-calist=['3C147','3C48','3C123','3C295','3C286','NGC7027']
-fluxlist=[5.1885,3.6722,11.0837,4.1610,5.7194,5.6107]
-colors=['k', 'b', 'r', 'g', 'c', 'm']
-colors = dict(zip(calist,colors))
+calist = ['3C147', '3C48', '3C123', '3C295', '3C286', 'NGC7027']
+fluxlist = np.array([5.1885, 3.6722, 11.0837, 4.1610, 5.7194, 5.6107])
+efluxlist = fluxlist * 0.05
+colors = ['k', 'b', 'r', 'g', 'c', 'm']
+colors = dict(zip(calist, colors))
 
-def calibration(config_file, channel='Ch0', feed=0):
+
+def get_fluxes(basedir, scandir, channel='Ch0', feed=0, plot=False):
+    """Get fluxes from all scans in path."""
+    dname = os.path.basename(scandir)
+    scan_list = \
+        list_scans(basedir, [scandir])
+
+    scan_list.sort()
+    output_table = Table(names=["Dir", "File", "Source", "Time", "Counts",
+                                "Counts Err", "Flux", "Flux Err", "Kind",
+                                "Elevation", "Flux/Counts",
+                                "Flux/Counts Err"],
+                         dtype=['U200', 'U200', 'U200', np.longdouble, np.float,
+                                np.float, np.float, np.float,
+                                "U200", np.float, np.float, np.float])
+    if plot:
+        fig = plt.figure(dname)
+
+    for s in scan_list:
+        sname = os.path.basename(s)
+        try:
+            scan = Scan(s, norefilt=True)
+        except:
+            print('{} is an invalid file'.format(s))
+            continue
+        ras = scan['ra'][:, feed]
+        decs = scan['dec'][:, feed]
+        time = np.mean(scan['time'][:])
+        el = np.degrees(np.mean(scan['el'][:, feed]))
+        source = scan.meta['SOURCE']
+        try:
+            index = calist.index(source)
+            flux = fluxlist[index]
+            eflux = efluxlist[index]
+            kind = "Calibrator"
+        except:
+            flux = 1.
+            eflux = 0
+            kind = "Source"
+
+        y = scan[channel]
+
+        ravar = np.max(ras) - np.min(ras)
+        decvar = np.max(decs) - np.min(decs)
+        if ravar > decvar:
+            x = ras
+        else:
+            x = decs
+        model, fit_info = fit_baseline_plus_bell(x, y, kind='gauss')
+
+        try:
+            uncert = fit_info['param_cov'].diagonal() ** 0.5
+        except:
+            print("fit failed")
+            continue
+        pars = model.parameters
+        pnames = model.param_names
+        counts = model.amplitude_1.value
+
+        index = pnames.index("amplitude_1")
+
+        counts_err = uncert[index]
+
+        if plot:
+            plt.plot(x - np.min(x), y)
+            plt.plot(x - np.min(x), model(x))
+            plt.title(source)
+
+        flux_over_counts = flux / counts
+        flux_over_counts_err = \
+            (counts_err / counts + eflux / flux) * flux_over_counts
+
+        output_table.add_row([scandir, sname, source, time, counts, counts_err,
+                              flux, eflux, kind, el, flux_over_counts,
+                              flux_over_counts_err])
+
+    if plot:
+        fig.savefig(dname + ".png")
+        plt.close(fig)
+    return output_table
+
+
+def get_full_table(config_file, channel='Ch0', feed=0, plot=False):
+    """Get all fluxes in the directories specified by the config file"""
     config = read_config(config_file)
 
     dir_list = config['list_of_directories']
     tables = {}
     for d in dir_list:
-        scan_list = \
-            list_scans(config['datadir'], [d])
-
-        scan_list.sort()
-
-        output_table = Table(names=["Source", "Time", "Amplitude", "Kind",
-                                    "Elevation", "Counts/flux",
-                                    "Counts/flux Err"],
-                             dtype=['S10', np.longdouble, np.float, "S10",
-                                    np.float, np.float, np.float])
-
-        for s in scan_list:
-            try:
-                scan = Scan(s, norefilt=True)
-            except:
-                print('{} is an invalid file'.format(s))
-                continue
-            ras = scan['ra'][:, feed]
-            decs = scan['dec'][:, feed]
-            time = np.mean(scan['time'][:])
-            el = np.degrees(np.mean(scan['el'][:, feed]))
-            source = scan.meta['SOURCE']
-            try:
-                index = calist.index(source)
-                flux = fluxlist[index]
-                kind = "Calibrator"
-            except:
-                flux = 1.
-                kind = "Source"
-
-            y = scan[channel]
-
-            ravar = np.max(ras) - np.min(ras)
-            decvar = np.max(decs) - np.min(decs)
-            if ravar > decvar:
-                x = ras
-            else:
-                x = decs
-            model, fit_info = fit_baseline_plus_bell(x, y, kind='gauss')
-
-            try:
-                uncert = fit_info['param_cov'].diagonal() ** 0.5
-            except:
-                print("fit failed")
-            pars = model.parameters
-            pnames = model.param_names
-            counts = model.amplitude_1.value
-
-            index = pnames.index("amplitude_1")
-
-            counts_err = uncert[index]
-            # plt.plot(x, y)
-            # plt.plot(x, model(x))
-            # plt.title(source)
-            # plt.show()
-
-            count_over_flux = flux / counts
-            count_over_flux_err = counts_err / counts * count_over_flux
-
-            output_table.add_row([source, time, counts, kind,
-                                  el, count_over_flux, count_over_flux_err])
+        output_table = get_fluxes(config['datadir'], d, channel=channel,
+                                  feed=feed, plot=plot)
         tables[d] = output_table
 
+    return Table(vstack(list(tables.values())))
+
+
+def show_calibration(config_file, channel='Ch0', feed=0, plotall=False):
+    """Show the results of calibration"""
+    full_table = get_full_table(config_file, channel=channel, feed=0,
+                                plot=plotall)
+
+    dir_list = list(set(full_table["Dir"]))
+
+    calibrator_table = full_table[full_table["Kind"] == "Calibrator"]
+
+    source_table = full_table[full_table["Kind"] == "Source"]
+
     for d in dir_list:
-        print(d)
-        print(tables[d])
-        if tables[d]["Kind"][0] == b"Source":
+        subtable = calibrator_table[calibrator_table["Dir"] == d]
+        if len(subtable) == 0:
             continue
 
-        plt.figure("Time evolution")
+        fig = plt.figure("Time evolution")
 
-        cf = np.mean(tables[d]["Counts/flux"])
-        cfe = np.sqrt(np.sum(tables[d]["Counts/flux Err"] ** 2)) / len(tables[d])
-        plt.errorbar(np.mean(tables[d]["Time"]), cf, yerr=cfe,
-                     ecolor=colors[tables[d]["Source"][0].decode()])
+        fc = np.mean(subtable["Flux/Counts"])
+        fce = np.sqrt(np.sum(subtable["Flux/Counts Err"] ** 2)) / len(subtable)
+        plt.errorbar(np.mean(subtable["Time"]), fc, yerr=fce,
+                     ecolor=colors[subtable["Source"][0]],
+                     elinewidth=3)
 
         plt.figure("Vs Elevation")
-        plt.errorbar(np.mean(tables[d]["Elevation"]), cf, yerr=cfe, ecolor=colors[tables[d]["Source"][0].decode()])
+        plt.errorbar(np.mean(subtable["Elevation"]), fc, yerr=fce,
+                     ecolor=colors[subtable["Source"][0]],
+                     elinewidth=3)
+
+        counts = np.mean(subtable["Counts"])
+        counts_err = \
+            np.sqrt(np.sum(subtable["Counts Err"] ** 2)) / len(subtable)
+        plt.figure("Vs Flux")
+        plt.errorbar(counts, fc, yerr=fce, xerr=counts_err,
+                     ecolor=colors[subtable["Source"][0]],
+                     elinewidth=3)
+
+    plt.figure("Time evolution")
+    plt.xlabel("Time")
+    plt.ylabel("Jansky / Counts")
+    plt.figure("Vs Elevation")
+    plt.ylabel("Jansky / Counts")
+    plt.xlabel("Elevation")
+    plt.figure("Vs Flux")
+    plt.ylabel("Jansky / Counts")
+    plt.xlabel("Flux")
+
+    fc = np.mean(calibrator_table["Flux/Counts"])
+    fce = np.sqrt(np.sum(calibrator_table["Flux/Counts Err"] ** 2))\
+        / len(calibrator_table)
+
+    source_table["Flux"] = source_table["Counts"] * fc
+    source_table["Flux Err"] = \
+        (source_table["Counts Err"] / source_table["Counts"] + fce / fc) * \
+        source_table["Flux"]
+
+    sources = list(set(source_table["Source"]))
+    for s in sources:
+        filtered = source_table[source_table["Source"] == s]
+        print(filtered[("Source", "Flux", "Flux Err", "Counts", "Counts Err")])
+        if len(filtered) > 20:
+            plt.figure(s)
+            plt.hist(filtered["Flux"], bins=len(filtered))
+            plt.xlabel("Flux values")
 
     plt.show()
 
-def test_calibration():
+
+def test_calibration_tp():
     curdir = os.path.abspath(os.path.dirname(__file__))
     config_file = \
         os.path.abspath(os.path.join(curdir, '..', '..',
                                      'TEST_DATASET',
                                      'test_calib.ini'))
-    calibration(config_file)
+    show_calibration(config_file)
+
+
+def test_calibration_roach():
+    curdir = os.path.abspath(os.path.dirname(__file__))
+    config_file = \
+        os.path.abspath(os.path.join(curdir, '..', '..',
+                                     'TEST_DATASET',
+                                     'test_calib_roach.ini'))
+    show_calibration(config_file)
