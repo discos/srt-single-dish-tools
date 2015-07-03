a3c92dd844e07aae9c046cdc71bb7078332eba39
mbachett <matteo.bachetti@irap.omp.eu>
2015-07-03 10:37:30 +0200

    Adds data files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-
+import os
 import glob
 import sys
 PY2 = sys.version_info[0] == 2
@@ -19,12 +19,14 @@ install_requires = [
 if PY2 and PYX6:
     install_requires += ['unittest2']
 
-print(install_requires)
+calibdir = os.path.join('data', 'calibrators')
+calibfiles = [(calibdir, [f for f in glob.glob(os.path.join(calibdir, '*.ini'))])]
 setup(name='srttools',
       version='0.1',
       description="SRT Single dish tools",
       packages=['srttools', 'srttools.core'],
-      package_data={'': ['README.md'], 'TEST_DATASET': ['*']},
+      package_data={'': ['README.md']},
+      data_files = calibfiles,
       include_package_data=True,
       author='OAC high-energygroup',
       author_email="blablabla@bla.net",
@@ -36,9 +38,6 @@ setup(name='srttools',
       classifiers=[
           'Intended Audience :: Science/Research, Education, Developers',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Topic :: Scientific/Engineering :: Astronomy'
           ],
