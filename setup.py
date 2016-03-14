60ecdb7f060fe868e031101d99362ed4b3574ca2
mbachett <matteo.bachetti@irap.omp.eu>
2016-03-14 15:01:26 +0100

    Add coveragerc to setup

--- setup.py
+++ setup.py
@@ -100,7 +100,8 @@ for entry_point in entry_point_list:
 c_files = []
 for root, dirs, files in os.walk(PACKAGENAME):
     for filename in files:
-        if filename.endswith('.c') or filename.endswith('.ini') or filename.endswith('.fits'):
+        if filename.endswith('.c') or filename.endswith('.ini') or filename.endswith('.fits') or \
+                filename == 'coveragerc':
             c_files.append(
                 os.path.join(
                     os.path.relpath(root, PACKAGENAME), filename))
