abd230721ddf07a081bc1beabc2580b5d8da799c
mbachett <matteo.bachetti@irap.omp.eu>
2016-03-16 11:50:39 +0100

    Convert from number of pixels to pixel size

--- srttools/core/read_config.py
+++ srttools/core/read_config.py
@@ -5,6 +5,7 @@ from __future__ import (absolute_import, unicode_literals, division,
 import os
 import glob
 import warnings
+import numpy as np
 # For Python 2 and 3 compatibility
 try:
     import configparser
@@ -44,9 +45,9 @@ def sample_config_file(fname='sample_config_file.ini'):
 ;    reference_ra : 10.5
 ;    reference_dec : 5.3
 
-;; Number of pixels, specified as pair x y
+;; Pixel size in arcminutes
 
-    npix : 30 30
+    pixel_size : 1
 
 ;; Channels to save from RFI filtering. It might indicate known strong spectral
 ;; lines
@@ -98,7 +99,7 @@ def read_config(fname=None):
     config_output['datadir'] = './'
     config_output['list_of_directories'] = '*'
     config_output['calibrator_directories'] = []
-    config_output['npix'] = '32 32'
+    config_output['pixel_size'] = '1'
     config_output['goodchans'] = None
     config_output['filtering_factor'] = '0'
 
@@ -145,7 +146,8 @@ def read_config(fname=None):
              for f in glob.glob(os.path.join(config_output['datadir'], '*'))
              if os.path.isdir(f)]  # only if it's a directory
 
-    config_output['npix'] = [int(n) for n in config_output['npix'].split()]
+    config_output['pixel_size'] = np.radians(float(config_output['pixel_size']) * 60)
+
     if config_output['goodchans'] is not None:
         config_output['goodchans'] = \
             [int(n) for n in config_output['goodchans']]
