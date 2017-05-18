08b3e36128896bf8f4fe7161f3fb021a3a17949e
Matteo Bachetti <matteo@matteobachetti.it>
2017-05-18 11:58:31 +0200

    Put back missing tests

--- srttools/core/tests/test_io_and_scan.py
+++ srttools/core/tests/test_io_and_scan.py
@@ -41,20 +41,20 @@ class Test1_Scan(object):
 
         read_config(klass.config_file)
 
-    # def test_print_info(self, capsys):
-    #     print_obs_info_fitszilla(self.fname)
-    #     out, err = capsys.readouterr()
-    #     assert 'bandwidth' in out.lower()
-    #
-    # def test_scan(self):
-    #     '''Test that data are read.'''
-    #
-    #     scan = Scan(self.fname)
-    #
-    #     scan.write('scan.hdf5', overwrite=True)
-    #     scan2 = Scan('scan.hdf5')
-    #     assert scan.meta == scan2.meta
-    #
+    def test_print_info(self, capsys):
+        print_obs_info_fitszilla(self.fname)
+        out, err = capsys.readouterr()
+        assert 'bandwidth' in out.lower()
+
+    def test_scan(self):
+        '''Test that data are read.'''
+
+        scan = Scan(self.fname)
+
+        scan.write('scan.hdf5', overwrite=True)
+        scan2 = Scan('scan.hdf5')
+        assert scan.meta == scan2.meta
+
     @pytest.mark.parametrize('fname', ['med_data.fits',
                                        'srt_data_tp_multif.fits'])
     def test_coordinate_conversion_works(self, fname):
@@ -72,67 +72,67 @@ class Test1_Scan(object):
         assert np.all(np.abs((altaz.alt.to(u.rad) - scan['el'][:,idx]).to(u.arcsec).value) < 1)
 
 
-    # @classmethod
-    # def teardown_class(klass):
-    #     """Cleanup."""
-    #     os.unlink('scan.hdf5')
-    #     for f in glob.glob(os.path.join(klass.datadir, '*.hdf5')):
-    #         os.unlink(f)
+    @classmethod
+    def teardown_class(klass):
+        """Cleanup."""
+        os.unlink('scan.hdf5')
+        for f in glob.glob(os.path.join(klass.datadir, '*.hdf5')):
+            os.unlink(f)
 
-#
-# class Test2_Scan(object):
-#     @classmethod
-#     def setup_class(klass):
-#         import os
-#         global DEBUG_MODE
-#         DEBUG_MODE = True
-#
-#         klass.curdir = os.path.dirname(__file__)
-#         klass.datadir = os.path.join(klass.curdir, 'data')
-#
-#         klass.fname = \
-#             os.path.abspath(
-#                 os.path.join(klass.datadir, 'spectrum',
-#                              'roach_template.fits'))
-#
-#         h5file = klass.fname.replace('.fits', '.hdf5')
-#         if os.path.exists(h5file):
-#             os.unlink(h5file)
-#         klass.config_file = \
-#             os.path.abspath(os.path.join(klass.datadir, 'spectrum.ini'))
-#         print(klass.config_file)
-#
-#         read_config(klass.config_file)
-#
-#     def test_scan(self):
-#         '''Test that data are read.'''
-#
-#         scan = Scan(self.fname)
-#
-#         scan.write('scan.hdf5', overwrite=True)
-#         scan.baseline_subtract('rough')
-#
-#     @pytest.mark.parametrize('fname', ['srt_data.fits'])
-#     def test_coordinate_conversion_works(self, fname):
-#         scan = Scan(os.path.join(self.datadir, 'spectrum', fname))
-#         obstimes = Time(scan['time'] * u.day, format='mjd', scale='utc')
-#         idx = 1 if '_multif' in fname else 0
-#         ref_coords = SkyCoord(ra=scan['ra'][:,idx],
-#                               dec=scan['dec'][:,idx],
-#                               obstime=obstimes,
-#                               location=locations[scan.meta['site']]
-#                               )
-#         altaz = ref_coords.altaz
-#
-#         assert np.all(np.abs((altaz.az.to(u.rad) - scan['az'][:,idx]).to(u.arcsec).value) < 1)
-#         assert np.all(np.abs((altaz.alt.to(u.rad) - scan['el'][:,idx]).to(u.arcsec).value) < 1)
-#
-#     @classmethod
-#     def teardown_class(klass):
-#         """Cleanup."""
-#         os.unlink('scan.hdf5')
-#         for f in glob.glob(os.path.join(klass.datadir, 'spectrum', '*.pdf')):
-#             os.unlink(f)
-#         for f in glob.glob(os.path.join(klass.datadir, 'spectrum', '*.hdf5')):
-#             os.unlink(f)
-#
+
+class Test2_Scan(object):
+    @classmethod
+    def setup_class(klass):
+        import os
+        global DEBUG_MODE
+        DEBUG_MODE = True
+
+        klass.curdir = os.path.dirname(__file__)
+        klass.datadir = os.path.join(klass.curdir, 'data')
+
+        klass.fname = \
+            os.path.abspath(
+                os.path.join(klass.datadir, 'spectrum',
+                             'roach_template.fits'))
+
+        h5file = klass.fname.replace('.fits', '.hdf5')
+        if os.path.exists(h5file):
+            os.unlink(h5file)
+        klass.config_file = \
+            os.path.abspath(os.path.join(klass.datadir, 'spectrum.ini'))
+        print(klass.config_file)
+
+        read_config(klass.config_file)
+
+    def test_scan(self):
+        '''Test that data are read.'''
+
+        scan = Scan(self.fname)
+
+        scan.write('scan.hdf5', overwrite=True)
+        scan.baseline_subtract('rough')
+
+    @pytest.mark.parametrize('fname', ['srt_data.fits'])
+    def test_coordinate_conversion_works(self, fname):
+        scan = Scan(os.path.join(self.datadir, 'spectrum', fname))
+        obstimes = Time(scan['time'] * u.day, format='mjd', scale='utc')
+        idx = 1 if '_multif' in fname else 0
+        ref_coords = SkyCoord(ra=scan['ra'][:,idx],
+                              dec=scan['dec'][:,idx],
+                              obstime=obstimes,
+                              location=locations[scan.meta['site']]
+                              )
+        altaz = ref_coords.altaz
+
+        assert np.all(np.abs((altaz.az.to(u.rad) - scan['az'][:,idx]).to(u.arcsec).value) < 1)
+        assert np.all(np.abs((altaz.alt.to(u.rad) - scan['el'][:,idx]).to(u.arcsec).value) < 1)
+
+    @classmethod
+    def teardown_class(klass):
+        """Cleanup."""
+        os.unlink('scan.hdf5')
+        for f in glob.glob(os.path.join(klass.datadir, 'spectrum', '*.pdf')):
+            os.unlink(f)
+        for f in glob.glob(os.path.join(klass.datadir, 'spectrum', '*.hdf5')):
+            os.unlink(f)
+
