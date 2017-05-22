0263defcb1ff7fd349a6311b2e5610e6bdde62a8
Matteo Bachetti <matteo@matteobachetti.it>
2017-05-22 10:37:51 +0200

    Change doctests to work

--- srttools/core/histograms.py
+++ srttools/core/histograms.py
@@ -350,7 +350,7 @@ def histogram2d(x, y, bins=10, range=None, normed=False, weights=None):
     Or we fill the histogram H with a determined bin content:
 
     >>> H = np.ones((4, 4)).cumsum().reshape(4, 4)
-    >>> print H[::-1]  # This shows the bin content in the order as plotted
+    >>> print(H[::-1])  # This shows the bin content in the order as plotted
     [[ 13.  14.  15.  16.]
      [  9.  10.  11.  12.]
      [  5.   6.   7.   8.]
@@ -360,31 +360,31 @@ def histogram2d(x, y, bins=10, range=None, normed=False, weights=None):
 
     >>> fig = plt.figure(figsize=(7, 3))
     >>> ax = fig.add_subplot(131)
-    >>> ax.set_title('imshow: equidistant')
+    >>> _ = ax.set_title('imshow: equidistant')
     >>> im = plt.imshow(H, interpolation='nearest', origin='low',
     ...                 extent=[xedges[0], xedges[-1], yedges[0], yedges[-1]])
 
     pcolormesh can display exact bin edges:
 
     >>> ax = fig.add_subplot(132)
-    >>> ax.set_title('pcolormesh: exact bin edges')
+    >>> _ = ax.set_title('pcolormesh: exact bin edges')
     >>> X, Y = np.meshgrid(xedges, yedges)
-    >>> ax.pcolormesh(X, Y, H)
-    >>> ax.set_aspect('equal')
+    >>> _ = ax.pcolormesh(X, Y, H)
+    >>> _ = ax.set_aspect('equal')
 
     NonUniformImage displays exact bin edges with interpolation:
 
     >>> ax = fig.add_subplot(133)
-    >>> ax.set_title('NonUniformImage: interpolated')
+    >>> _ = ax.set_title('NonUniformImage: interpolated')
     >>> im = mpl.image.NonUniformImage(ax, interpolation='bilinear')
     >>> xcenters = xedges[:-1] + 0.5 * (xedges[1:] - xedges[:-1])
     >>> ycenters = yedges[:-1] + 0.5 * (yedges[1:] - yedges[:-1])
-    >>> im.set_data(xcenters, ycenters, H)
-    >>> ax.images.append(im)
-    >>> ax.set_xlim(xedges[0], xedges[-1])
-    >>> ax.set_ylim(yedges[0], yedges[-1])
-    >>> ax.set_aspect('equal')
-    >>> plt.show()
+    >>> _ = im.set_data(xcenters, ycenters, H)
+    >>> _ = ax.images.append(im)
+    >>> _ = ax.set_xlim(xedges[0], xedges[-1])
+    >>> _ = ax.set_ylim(yedges[0], yedges[-1])
+    >>> _ = ax.set_aspect('equal')
+    >>> _ = plt.close(fig)
 
     """
     try:
