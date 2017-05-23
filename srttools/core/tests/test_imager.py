340298ec234b62c8041b8e2884ae92c129781661
Matteo Bachetti <matteo@matteobachetti.it>
2017-05-23 18:01:11 +0200

    Simulate test dataset during test_imager

--- srttools/core/tests/test_imager.py
+++ srttools/core/tests/test_imager.py
@@ -16,13 +16,20 @@ import os
 import glob
 import subprocess as sp
 
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(x):
+        return x
+
+
 
 np.random.seed(1241347)
 
 
 def _2d_gauss(x, y, sigma=3 / 60.):
     """A Gaussian beam"""
-    return np.exp(-(x ** 2 + y ** 2) / sigma**2)
+    return np.exp(-(x ** 2 + y ** 2) / (2 * sigma**2))
 
 
 def gauss_src_func(x, y):
@@ -52,7 +59,7 @@ calibrator_directories :
 
 noise_threshold : 5
 
-pixel_size : 1
+pixel_size : 0.8
         """
     with open(filename, 'w') as fobj:
         print(string, file=fobj)
@@ -61,10 +68,10 @@ pixel_size : 1
 
 def sim_map(obsdir_ra, obsdir_dec):
     simulate_map(count_map=gauss_src_func,
-                 length_ra=10. * np.cos(np.radians(70)),
-                 length_dec=10.,
+                 length_ra=30.,
+                 length_dec=30.,
                  outdir=(obsdir_ra, obsdir_dec), mean_ra=180,
-                 mean_dec=70,
+                 mean_dec=70, speed=2.,
                  spacing=0.5, srcname='Dummy')
 
 
@@ -72,13 +79,16 @@ def sim_calibrators(ncross, caldir):
     src_ra = 185
     src_dec = 75
     timedelta = 0
-    scan_values = np.arange(-5, 5, 0.002)
+    speed = 2.  # arcmin/s
+    dt = 0.04
+    dtheta = speed * dt
+    scan_values = np.arange(-2, 2, dtheta/60)
     zero_values = np.zeros_like(scan_values)
 
-    for i in range(ncross):
+    for i in tqdm(range(ncross)):
         ras = src_ra + scan_values / np.cos(np.radians(src_dec))
         decs = src_dec + zero_values
-        times = np.arange(scan_values.size) * 0.04 + timedelta
+        times = np.arange(scan_values.size) * dt + timedelta
 
         scan = calibrator_scan_func(scan_values) + \
                ra.normal(0, 0.2, scan_values.size)
@@ -89,7 +99,7 @@ def sim_calibrators(ncross, caldir):
 
         ras = src_ra + zero_values
         decs = src_dec + scan_values
-        times = np.arange(scan_values.size) * 0.04 + timedelta
+        times = np.arange(scan_values.size) * dt + timedelta
 
         scan = calibrator_scan_func(scan_values) + \
                ra.normal(0, 0.2, scan_values.size)
@@ -119,9 +129,22 @@ class TestScanSet(object):
         mkdir_p(klass.caldir)
         # First off, simulate a beamed observation  -------
 
+        print('Setting up simulated data.')
         sim_config_file(klass.config_file)
+        print('Fake calibrators: DummyCal, 1 Jy.')
         sim_calibrators(5, klass.caldir)
+        print('Fake map: Point-like (but Gaussian beam shape), 0.5 Jy.')
         sim_map(klass.obsdir_ra, klass.obsdir_dec)
+
+        caltable = CalibratorTable()
+        caltable.from_scans(glob.glob(os.path.join(klass.caldir,
+                                                   '*.fits')))
+
+        caltable.update()
+        klass.calfile = os.path.join(klass.datadir, 'calibrators.hdf5')
+        caltable.write(klass.calfile,
+                       path="config", overwrite=True)
+
 
         klass.config = read_config(klass.config_file)
         klass.scanset = ScanSet(klass.config_file, norefilt=False)
@@ -221,19 +244,11 @@ class TestScanSet(object):
     def test_6_calibrate_image(self):
         scanset = ScanSet(Table.read('test.hdf5', path='scanset'),
                           config_file=self.config_file)
-        caltable = CalibratorTable()
-        caltable.from_scans(glob.glob(os.path.join(self.datadir,
-                                                   'calibrators',
-                                                   'cal*.fits')))
-
-        caltable.update()
-        calfile = os.path.join(self.datadir, 'calibrators.hdf5')
-        caltable.write(calfile,
-                       path="config", overwrite=True)
         images = scanset.calculate_images()
 
-        images = scanset.calculate_images(calibration=calfile)
-        assert np.allclose(np.sum(images['Ch0']), 1)
+        images = scanset.calculate_images(calibration=self.calfile)
+
+        assert np.allclose(np.sum(images['Ch0']), 0.5)
 
     def test_7_ds9_image(self):
         '''Test image production.'''
@@ -273,7 +288,7 @@ class TestScanSet(object):
         os.unlink('delta_altaz.png')
         os.unlink('altaz_with_src.png')
         os.unlink('img_sdev.png')
-        os.unlink('test.hdf5')
+        # os.unlink('test.hdf5')
         for d in klass.config['list_of_directories']:
             hfiles = \
                 glob.glob(os.path.join(klass.config['datadir'], d, '*.hdf5'))
@@ -284,5 +299,8 @@ class TestScanSet(object):
             os.unlink(o)
         out_fits_files = glob.glob(os.path.join(klass.config['datadir'],
                                                 'test_config*.fits'))
-        for o in out_fits_files:
+        out_hdf5_files = glob.glob(os.path.join(klass.config['datadir'], 'sim',
+                                                '*/', '*.hdf5'))
+
+        for o in out_fits_files + out_hdf5_files:
             os.unlink(o)
