95ec2fc0562f2b9bb1341d12b77ddade4022bd86
Matteo Bachetti <matteo@matteobachetti.it>
2017-05-24 12:20:39 +0200

    Check that calibrator fits are precise (for Gaussian beam)

--- srttools/core/tests/test_calibration.py
+++ srttools/core/tests/test_calibration.py
@@ -2,11 +2,66 @@ from __future__ import division, print_function
 from ..calibration import CalibratorTable
 from ..read_config import read_config
 from ..scan import list_scans
+from ..simulate import save_scan
+from ..io import mkdir_p
 import pytest
 
 import os
 import glob
 import shutil
+import numpy as np
+import numpy.random as ra
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(x):
+        return x
+
+
+def _2d_gauss(x, y, sigma=3 / 60.):
+    """A Gaussian beam"""
+    return np.exp(-(x ** 2 + y ** 2) / (2 * sigma**2))
+
+np.random.seed(1241347)
+
+
+def calibrator_scan_func(x):
+    return 100 * _2d_gauss(x, 0, sigma=3 / 60)
+
+
+def sim_calibrators(ncross, caldir):
+    src_ra = 185
+    src_dec = 75
+    timedelta = 0
+    speed = 2.  # arcmin/s
+    dt = 0.04
+    dtheta = speed * dt
+    scan_values = np.arange(-2, 2, dtheta/60)
+    zero_values = np.zeros_like(scan_values)
+
+    for i in tqdm(range(ncross)):
+        ras = src_ra + scan_values / np.cos(np.radians(src_dec))
+        decs = src_dec + zero_values
+        times = np.arange(scan_values.size) * dt + timedelta
+
+        scan = calibrator_scan_func(scan_values) + \
+               ra.normal(0, 0.2, scan_values.size)
+        save_scan(times, ras, decs, {'Ch0': scan, 'Ch1': scan},
+                  filename=os.path.join(caldir, '{}_Ra.fits'.format(i)),
+                  src_ra=src_ra, src_dec=src_dec, srcname='DummyCal')
+        timedelta = times[-1] + 1
+
+        ras = src_ra + zero_values
+        decs = src_dec + scan_values
+        times = np.arange(scan_values.size) * dt + timedelta
+
+        scan = calibrator_scan_func(scan_values) + \
+               ra.normal(0, 0.2, scan_values.size)
+        save_scan(times, ras, decs, {'Ch0': scan, 'Ch1': scan},
+                  filename=os.path.join(caldir, '{}_Dec.fits'.format(i)),
+                  src_ra=src_ra, src_dec=src_dec, srcname='DummyCal')
+        timedelta = times[-1] + 1
 
 
 class TestCalibration(object):
@@ -23,10 +78,13 @@ class TestCalibration(object):
             os.path.abspath(os.path.join(klass.datadir, "calibrators.ini"))
 
         klass.config = read_config(klass.config_file)
+        klass.caldir = os.path.join(klass.datadir, 'sim', 'calibration')
+        mkdir_p(klass.caldir)
+        # print('Fake calibrators: DummyCal, 1 Jy.')
+        # sim_calibrators(5, klass.caldir)
 
         klass.scan_list = \
-            list_scans(klass.config['datadir'],
-                       klass.config['calibrator_directories'])
+            list_scans(klass.caldir, ['./'])
 
         klass.scan_list.sort()
 
@@ -48,16 +106,27 @@ class TestCalibration(object):
                                               'calibrators', 'bubu.fits')])
         assert "Error while processing" in record[0].message.args[0]
 
-    def test_calibration(self):
+    def test_calibration_counts(self):
         """Simple calibration from scans."""
 
         caltable = CalibratorTable()
         caltable.from_scans(self.scan_list)
 
         caltable.update()
-        caltable.Jy_over_counts('Ch0')
-        caltable.Jy_over_counts('Ch0', elevation=19)
-        caltable.counts_over_Jy('Ch0')
+
+        assert np.all(
+            np.abs(caltable['Counts'] - 100.) < 3 * caltable['Counts Err'])
+
+    def test_calibration_width(self):
+        """Simple calibration from scans."""
+
+        caltable = CalibratorTable()
+        caltable.from_scans(self.scan_list)
+
+        caltable.update()
+
+        assert np.all(
+            np.abs(caltable['Width'] - 3/60.) < 3 * caltable['Width Err'])
 
     def test_calibration_write_and_plot(self):
         """Simple calibration from scans."""
