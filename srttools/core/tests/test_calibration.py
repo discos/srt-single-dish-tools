80b834b5b314d42f43ab6aa25d89906fe6055752
Matteo Bachetti <matteo@matteobachetti.it>
2017-06-05 15:26:42 +0200

    Calculate flux from cross scans

--- srttools/core/tests/test_calibration.py
+++ srttools/core/tests/test_calibration.py
@@ -4,6 +4,7 @@ from ..read_config import read_config
 from ..scan import list_scans
 from ..simulate import save_scan
 from ..io import mkdir_p
+from ..utils import compare_strings
 import pytest
 
 import os
@@ -30,7 +31,12 @@ def calibrator_scan_func(x):
     return 100 * _2d_gauss(x, 0, sigma=3 / 60)
 
 
-def sim_calibrators(ncross, caldir):
+def source_scan_func(x):
+    return 52 * _2d_gauss(x, 0, sigma=3 / 60)
+
+
+def sim_crossscans(ncross, caldir, scan_func=calibrator_scan_func,
+                   srcname='DummyCal'):
     src_ra = 185
     src_dec = 75
     timedelta = 0
@@ -45,22 +51,22 @@ def sim_calibrators(ncross, caldir):
         decs = src_dec + zero_values
         times = np.arange(scan_values.size) * dt + timedelta
 
-        scan = calibrator_scan_func(scan_values) + \
+        scan = scan_func(scan_values) + \
             ra.normal(0, 0.2, scan_values.size)
         save_scan(times, ras, decs, {'Ch0': scan, 'Ch1': scan},
                   filename=os.path.join(caldir, '{}_Ra.fits'.format(i)),
-                  src_ra=src_ra, src_dec=src_dec, srcname='DummyCal')
+                  src_ra=src_ra, src_dec=src_dec, srcname=srcname)
         timedelta = times[-1] + 1
 
         ras = src_ra + zero_values
         decs = src_dec + scan_values
         times = np.arange(scan_values.size) * dt + timedelta
 
-        scan = calibrator_scan_func(scan_values) + \
+        scan = scan_func(scan_values) + \
             ra.normal(0, 0.2, scan_values.size)
         save_scan(times, ras, decs, {'Ch0': scan, 'Ch1': scan},
                   filename=os.path.join(caldir, '{}_Dec.fits'.format(i)),
-                  src_ra=src_ra, src_dec=src_dec, srcname='DummyCal')
+                  src_ra=src_ra, src_dec=src_dec, srcname=srcname)
         timedelta = times[-1] + 1
 
 
@@ -79,20 +85,29 @@ class TestCalibration(object):
 
         klass.config = read_config(klass.config_file)
         klass.caldir = os.path.join(klass.datadir, 'sim', 'calibration')
+        klass.crossdir = os.path.join(klass.datadir, 'sim', 'crossscans')
         if not os.path.exists(klass.caldir):
             print('Fake calibrators: DummyCal, 1 Jy.')
             mkdir_p(klass.caldir)
-            sim_calibrators(5, klass.caldir)
+            sim_crossscans(5, klass.caldir)
+        if not os.path.exists(klass.crossdir):
+            print('Fake cross scans: DummySrc, 0.52 Jy.')
+            mkdir_p(klass.crossdir)
+            sim_crossscans(5, klass.crossdir, srcname='DummySrc',
+                           scan_func=source_scan_func)
 
         klass.scan_list = \
-            list_scans(klass.caldir, ['./'])
+            list_scans(klass.caldir, ['./']) + \
+            list_scans(klass.crossdir, ['./'])
 
         klass.scan_list.sort()
         caltable = CalibratorTable()
         caltable.from_scans(klass.scan_list)
         caltable.update()
+
         klass.calfile = os.path.join(klass.curdir, 'test_calibration.hdf5')
         caltable.write(klass.calfile, overwrite=True)
+        caltable.write(klass.calfile.replace('hdf5', 'csv'))
 
     def test_0_prepare(self):
         pass
@@ -123,6 +138,7 @@ class TestCalibration(object):
         """Simple calibration from scans."""
 
         caltable = CalibratorTable.read(self.calfile)
+        caltable = caltable[compare_strings(caltable['Source'], 'DummyCal')]
         assert np.all(
             np.abs(caltable['Counts'] - 100.) < 3 * caltable['Counts Err'])
 
@@ -141,6 +157,13 @@ class TestCalibration(object):
 
         caltable = CalibratorTable.read(self.calfile)
         caltable.show()
+
+    def test_calibrated_crossscans(self):
+        caltable = CalibratorTable.read(self.calfile)
+        dummy_flux, dummy_flux_err = \
+            caltable.calculate_src_flux(source='DummySrc')
+        assert (dummy_flux - 0.52) < 3 * dummy_flux_err
+
 
     @classmethod
     def teardown_class(klass):
