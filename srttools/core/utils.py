Correct mad formula

srttools/core/utils.py
@@ -6,10 +6,13 @@ import numpy as np
 
 
 try:
-    from statsmodels.robust import mad
+    from statsmodels.robust import stand_mad as mad
 except ImportError:
-    def mad(data, axis=None):
-        return np.median(np.abs(data - np.median(data, axis)), axis)
+    def mad(data, c=0.6745, axis=None):
+        """Straight from statsmodels's source code"""
+        data = np.asarray(data)
+        center = np.apply_over_axes(np.median, data, axis)
+        return np.median((np.fabs(data - center)) / c, axis=axis)
 
 
 def standard_string(s):