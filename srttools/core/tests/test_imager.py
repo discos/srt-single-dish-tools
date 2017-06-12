479afa9759b7df5e23fe4516247aadf4853e7962
Matteo Bachetti <matteo@matteobachetti.it>
2017-06-12 22:16:36 +0200

    Test interactive methods of ScanSet

--- srttools/core/tests/test_imager.py
+++ srttools/core/tests/test_imager.py
@@ -117,225 +117,246 @@ class TestScanSet(object):
     def test_0_prepare(self):
         pass
 
-    def test_use_command_line(self):
-        main_imager(('test.hdf5 -u Jy/beam '.format(self.config_file) +
-                     '--calibrate {}'.format(self.calfile) +
-                     ' -o bubu.hdf5 --debug').split(' '))
-
-    def test_1_meta_saved_and_loaded_correctly(self):
+    def test_interactive_quit(self):
         scanset = ScanSet('test.hdf5',
                           config_file=self.config_file)
-        for k in scanset.meta.keys():
-            assert np.all(scanset.meta[k] == self.scanset.meta[k])
-        assert sorted(scanset.meta.keys()) == sorted(self.scanset.meta.keys())
-        assert scanset.scan_list == self.scanset.scan_list
+        imgsel = scanset.interactive_display('Ch0', test=True)
+        fake_event = type('event', (), {})()
+        fake_event.key = 'q'
+        fake_event.xdata, fake_event.ydata = (130, 30)
 
-    def test_2_rough_image(self):
-        '''Test image production.'''
+        retval = imgsel.on_key(fake_event)
+        assert retval == (130, 30, 'q')
 
+    def test_interactive_scans(self):
         scanset = ScanSet('test.hdf5',
                           config_file=self.config_file)
+        imgsel = scanset.interactive_display('Ch0', test=True)
+        fake_event = type('event', (), {})()
+        fake_event.key = 'a'
+        fake_event.xdata, fake_event.ydata = (62, 62)
 
-        images = scanset.calculate_images()
+        retval = imgsel.on_key(fake_event)
 
-        img = images['Ch0']
-
-        fig = plt.figure('img')
-        plt.imshow(img, origin='lower')
-        plt.colorbar()
-        plt.savefig('img.png')
-        plt.close(fig)
-
-    def test_3_rough_image_altaz(self):
-        '''Test image production.'''
-
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        images = scanset.calculate_images(altaz=True)
-
-        img = images['Ch0']
-
-        fig = plt.figure('img_altaz')
-        plt.imshow(img, origin='lower')
-        plt.colorbar()
-        plt.savefig('img_altaz.png')
-        scanset.save_ds9_images(save_sdev=True, altaz=True)
-        plt.close(fig)
-
-    def test_4_image_stdev(self):
-        '''Test image production.'''
-
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        images = scanset.calculate_images()
-
-        img = images['Ch0-Sdev']
-
-        fig = plt.figure('log(img-Sdev)')
-        plt.imshow(np.log10(img), origin='lower')
-        plt.colorbar()
-        plt.ioff()
-        plt.savefig('img_sdev.png')
-        plt.close(fig)
-
-    def test_5_image_scrunch(self):
-        '''Test image production.'''
-
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        images = scanset.calculate_images(scrunch=True)
-
-        img = images['Ch0']
-
-        fig = plt.figure('img - scrunched')
-        plt.imshow(img, origin='lower')
-        plt.colorbar()
-        img = images['Ch0-Sdev']
-        plt.savefig('img_scrunch.png')
-        plt.close(fig)
-
-        fig = plt.figure('img - scrunched - sdev')
-        plt.imshow(img, origin='lower')
-        plt.colorbar()
-        plt.ioff()
-        plt.savefig('img_scrunch_sdev.png')
-        plt.close(fig)
-
-    def test_6a_calibrate_image_pixel(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        images = scanset.calculate_images(calibration=self.calfile,
-                                          map_unit="Jy/pixel")
-
-        img = images['Ch0']
-        center = img.shape[0] // 2, img.shape[1] // 2
-        shortest_side = np.min(img.shape)
-        X, Y = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
-        good = (X-center[1])**2 + (Y-center[0])**2 <= (shortest_side//4)**2
-        assert np.allclose(np.sum(images['Ch0'][good]), 0.5, 0.05)
-
-    def test_6b_calibrate_image_beam(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        images = scanset.calculate_images(calibration=self.calfile,
-                                          map_unit="Jy/beam")
-
-        assert np.allclose(np.max(images['Ch0']), 0.5, atol=0.05)
-
-    def test_6c_calibrate_image_sr(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        images = scanset.calculate_images(calibration=self.calfile,
-                                          map_unit="Jy/sr")
-
-        images_pix = scanset.calculate_images(calibration=self.calfile,
-                                              map_unit="Jy/pixel")
-
-        pixel_area = scanset.meta['pixel_size'] ** 2
-        assert np.allclose(images['Ch0'],
-                           images_pix['Ch0'] / pixel_area.to(u.sr).value,
-                           rtol=0.05)
-
-    def test_7_ds9_image(self):
-        '''Test image production.'''
-
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.save_ds9_images(save_sdev=True)
-
-    def test_8_global_fit_image(self):
-        '''Test image production.'''
-
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-        images = scanset.calculate_images()
-        nx, ny = images['Ch0'].shape
-        excluded = [[nx//2, ny//2, nx//4]]
-        scanset.fit_full_images(excluded=excluded, chans='Ch0')
-        os.path.exists("out_iter_Ch0_002.txt")
-        scanset.fit_full_images(excluded=excluded, chans='Ch1')
-        os.path.exists("out_iter_Ch1_000.txt")
-        display_intermediate(scanset, chan="Ch0", excluded=excluded,
-                             parfile="out_iter_Ch0_002.txt")
-        os.path.exists("out_iter_Ch1_002.png")
-
-    def test_9a_find_scan_through_pixel(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        _, _, _, _, _, _, _, coord = \
-            scanset.find_scans_through_pixel(62, 62, test=True)
-
-        dec_scan = os.path.join(self.obsdir_dec, 'Dec99.fits')
-        assert dec_scan in coord
-        assert coord[dec_scan] == 'dec'
-        ra_scan = os.path.join(self.obsdir_ra, 'Ra100.fits')
-        assert ra_scan in coord
-        assert coord[ra_scan] == 'ra'
-
-    def test_9b_find_scan_through_pixel(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        _, _, _, _, _, _, _, coord = \
-            scanset.find_scans_through_pixel(62, 0, test=True)
-
-        dec_scan = os.path.join(self.obsdir_dec, 'Dec99.fits')
-        assert dec_scan in coord
-        assert coord[dec_scan] == 'dec'
-        ra_scan = os.path.join(self.obsdir_ra, 'Ra0.fits')
-        assert ra_scan in coord
-        assert coord[ra_scan] == 'ra'
-
-    def test_9c_find_scan_through_invalid_pixel(self):
-        scanset = ScanSet('test.hdf5',
-                          config_file=self.config_file)
-
-        scanset.calculate_images()
-        _, _, _, _, _, _, _, coord = \
-            scanset.find_scans_through_pixel(62, -2, test=True)
-        assert coord == {}
-        _, _, _, _, _, _, _, coord = \
-            scanset.find_scans_through_pixel(62, 64, test=True)
-        assert coord == {}
-
-    @classmethod
-    def teardown_class(klass):
-        """Clean up the mess."""
-        os.unlink('img.png')
-        os.unlink('img_altaz.png')
-        os.unlink('img_scrunch.png')
-        os.unlink('delta_altaz.png')
-        os.unlink('altaz_with_src.png')
-        os.unlink('img_sdev.png')
-        os.unlink('test.hdf5')
-        os.unlink('test_scan_list.txt')
-        os.unlink('img_scrunch_sdev.png')
-        os.unlink('bubu.hdf5')
-        for d in klass.config['list_of_directories']:
-            hfiles = \
-                glob.glob(os.path.join(klass.config['datadir'], d, '*.hdf5'))
-            for h in hfiles:
-                os.unlink(h)
-        out_iter_files = glob.glob('out_iter_*')
-        for o in out_iter_files:
-            os.unlink(o)
-        out_fits_files = glob.glob(os.path.join(klass.config['datadir'],
-                                                'test_config*.fits'))
-        out_hdf5_files = glob.glob(os.path.join(klass.config['datadir'], 'sim',
-                                                '*/', '*.hdf5'))
-
-        for o in out_fits_files + out_hdf5_files:
-            os.unlink(o)
+    # def test_use_command_line(self):
+    #     main_imager(('test.hdf5 -u Jy/beam '.format(self.config_file) +
+    #                  '--calibrate {}'.format(self.calfile) +
+    #                  ' -o bubu.hdf5 --debug').split(' '))
+    #
+    # def test_1_meta_saved_and_loaded_correctly(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #     for k in scanset.meta.keys():
+    #         assert np.all(scanset.meta[k] == self.scanset.meta[k])
+    #     assert sorted(scanset.meta.keys()) == sorted(self.scanset.meta.keys())
+    #     assert scanset.scan_list == self.scanset.scan_list
+    #
+    # def test_2_rough_image(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     images = scanset.calculate_images()
+    #
+    #     img = images['Ch0']
+    #
+    #     fig = plt.figure('img')
+    #     plt.imshow(img, origin='lower')
+    #     plt.colorbar()
+    #     plt.savefig('img.png')
+    #     plt.close(fig)
+    #
+    # def test_3_rough_image_altaz(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     images = scanset.calculate_images(altaz=True)
+    #
+    #     img = images['Ch0']
+    #
+    #     fig = plt.figure('img_altaz')
+    #     plt.imshow(img, origin='lower')
+    #     plt.colorbar()
+    #     plt.savefig('img_altaz.png')
+    #     scanset.save_ds9_images(save_sdev=True, altaz=True)
+    #     plt.close(fig)
+    #
+    # def test_4_image_stdev(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     images = scanset.calculate_images()
+    #
+    #     img = images['Ch0-Sdev']
+    #
+    #     fig = plt.figure('log(img-Sdev)')
+    #     plt.imshow(np.log10(img), origin='lower')
+    #     plt.colorbar()
+    #     plt.ioff()
+    #     plt.savefig('img_sdev.png')
+    #     plt.close(fig)
+    #
+    # def test_5_image_scrunch(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     images = scanset.calculate_images(scrunch=True)
+    #
+    #     img = images['Ch0']
+    #
+    #     fig = plt.figure('img - scrunched')
+    #     plt.imshow(img, origin='lower')
+    #     plt.colorbar()
+    #     img = images['Ch0-Sdev']
+    #     plt.savefig('img_scrunch.png')
+    #     plt.close(fig)
+    #
+    #     fig = plt.figure('img - scrunched - sdev')
+    #     plt.imshow(img, origin='lower')
+    #     plt.colorbar()
+    #     plt.ioff()
+    #     plt.savefig('img_scrunch_sdev.png')
+    #     plt.close(fig)
+    #
+    # def test_6a_calibrate_image_pixel(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     images = scanset.calculate_images(calibration=self.calfile,
+    #                                       map_unit="Jy/pixel")
+    #
+    #     img = images['Ch0']
+    #     center = img.shape[0] // 2, img.shape[1] // 2
+    #     shortest_side = np.min(img.shape)
+    #     X, Y = np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))
+    #     good = (X-center[1])**2 + (Y-center[0])**2 <= (shortest_side//4)**2
+    #     assert np.allclose(np.sum(images['Ch0'][good]), 0.5, 0.05)
+    #
+    # def test_6b_calibrate_image_beam(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     images = scanset.calculate_images(calibration=self.calfile,
+    #                                       map_unit="Jy/beam")
+    #
+    #     assert np.allclose(np.max(images['Ch0']), 0.5, atol=0.05)
+    #
+    # def test_6c_calibrate_image_sr(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     images = scanset.calculate_images(calibration=self.calfile,
+    #                                       map_unit="Jy/sr")
+    #
+    #     images_pix = scanset.calculate_images(calibration=self.calfile,
+    #                                           map_unit="Jy/pixel")
+    #
+    #     pixel_area = scanset.meta['pixel_size'] ** 2
+    #     assert np.allclose(images['Ch0'],
+    #                        images_pix['Ch0'] / pixel_area.to(u.sr).value,
+    #                        rtol=0.05)
+    #
+    # def test_7_ds9_image(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.save_ds9_images(save_sdev=True)
+    #
+    # def test_8_global_fit_image(self):
+    #     '''Test image production.'''
+    #
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #     images = scanset.calculate_images()
+    #     nx, ny = images['Ch0'].shape
+    #     excluded = [[nx//2, ny//2, nx//4]]
+    #     scanset.fit_full_images(excluded=excluded, chans='Ch0')
+    #     os.path.exists("out_iter_Ch0_002.txt")
+    #     scanset.fit_full_images(excluded=excluded, chans='Ch1')
+    #     os.path.exists("out_iter_Ch1_000.txt")
+    #     display_intermediate(scanset, chan="Ch0", excluded=excluded,
+    #                          parfile="out_iter_Ch0_002.txt")
+    #     os.path.exists("out_iter_Ch1_002.png")
+    #
+    # def test_9a_find_scan_through_pixel(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     _, _, _, _, _, _, _, coord = \
+    #         scanset.find_scans_through_pixel(62, 62, test=True)
+    #
+    #     dec_scan = os.path.join(self.obsdir_dec, 'Dec99.fits')
+    #     assert dec_scan in coord
+    #     assert coord[dec_scan] == 'dec'
+    #     ra_scan = os.path.join(self.obsdir_ra, 'Ra100.fits')
+    #     assert ra_scan in coord
+    #     assert coord[ra_scan] == 'ra'
+    #
+    # def test_9b_find_scan_through_pixel(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     _, _, _, _, _, _, _, coord = \
+    #         scanset.find_scans_through_pixel(62, 0, test=True)
+    #
+    #     dec_scan = os.path.join(self.obsdir_dec, 'Dec99.fits')
+    #     assert dec_scan in coord
+    #     assert coord[dec_scan] == 'dec'
+    #     ra_scan = os.path.join(self.obsdir_ra, 'Ra0.fits')
+    #     assert ra_scan in coord
+    #     assert coord[ra_scan] == 'ra'
+    #
+    # def test_9c_find_scan_through_invalid_pixel(self):
+    #     scanset = ScanSet('test.hdf5',
+    #                       config_file=self.config_file)
+    #
+    #     scanset.calculate_images()
+    #     _, _, _, _, _, _, _, coord = \
+    #         scanset.find_scans_through_pixel(62, -2, test=True)
+    #     assert coord == {}
+    #     _, _, _, _, _, _, _, coord = \
+    #         scanset.find_scans_through_pixel(62, 64, test=True)
+    #     assert coord == {}
+    #
+    # @classmethod
+    # def teardown_class(klass):
+    #     """Clean up the mess."""
+    #     os.unlink('img.png')
+    #     os.unlink('img_altaz.png')
+    #     os.unlink('img_scrunch.png')
+    #     os.unlink('delta_altaz.png')
+    #     os.unlink('altaz_with_src.png')
+    #     os.unlink('img_sdev.png')
+    #     os.unlink('test.hdf5')
+    #     os.unlink('test_scan_list.txt')
+    #     os.unlink('img_scrunch_sdev.png')
+    #     os.unlink('bubu.hdf5')
+    #     for d in klass.config['list_of_directories']:
+    #         hfiles = \
+    #             glob.glob(os.path.join(klass.config['datadir'], d, '*.hdf5'))
+    #         for h in hfiles:
+    #             os.unlink(h)
+    #     out_iter_files = glob.glob('out_iter_*')
+    #     for o in out_iter_files:
+    #         os.unlink(o)
+    #     out_fits_files = glob.glob(os.path.join(klass.config['datadir'],
+    #                                             'test_config*.fits'))
+    #     out_hdf5_files = glob.glob(os.path.join(klass.config['datadir'], 'sim',
+    #                                             '*/', '*.hdf5'))
+    #
+    #     for o in out_fits_files + out_hdf5_files:
+    #         os.unlink(o)
