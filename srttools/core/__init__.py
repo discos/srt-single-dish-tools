b9ccbb2addd8dcaeb100bb5e95768caa2a97c280
Matteo Bachetti <matteo@matteobachetti.it>
2017-06-12 17:21:51 +0200

    Set default backend, and minimum statsmodels version

--- srttools/core/__init__.py
+++ srttools/core/__init__.py
@@ -1,14 +1,23 @@
 import warnings
+DEFAULT_MPL_BACKEND = 'TkAgg'
 try:
     import matplotlib
 
-    # matplotlib.use('TkAgg')
+    # This is necessary. Random backends might respond incorrectly.
+    matplotlib.use(DEFAULT_MPL_BACKEND)
     HAS_MPL = True
 except ImportError:
     HAS_MPL = False
 
 try:
     import statsmodels.api as sm
+    version = [int(i) for i in sm.version.version.split('.')]
+
+    # Minimum version 0.8.0
+    if version < (0, 8, 0):
+        warnings.warn("Please update statsmodels")
+        raise ImportError
+
     HAS_STATSM = True
 except ImportError:
     HAS_STATSM = False
