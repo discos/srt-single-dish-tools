e8aef51b63e53c8d4b5cec4665e7b3a7ed03ad63
Matteo Bachetti <matteo@matteobachetti.it>
2017-06-16 13:59:32 +0200

    Actual test

--- srttools/tests/test_import.py
+++ srttools/tests/test_import.py
@@ -1,8 +1,17 @@
 from srttools import Scan, ScanSet, CalibratorTable
+from astropy.table import Table
+
 
 def test_import_scan():
     s = Scan()
+    assert isinstance(s, Table)
+
+
 def test_import_scanset():
     s = ScanSet()
+    assert isinstance(s, Table)
+
+
 def test_import_calibratortable():
     s = CalibratorTable()
+    assert isinstance(s, Table)
