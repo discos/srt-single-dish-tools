16061c33f3d81fce9de9eaab556a9152e37ce839
Matteo Bachetti <matteo@matteobachetti.it>
2017-06-22 14:42:45 +0200

    Use local setup_conda

---
+++ setup_conda_linux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install conda
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+
+# This if accounts for a cached installation of anaconda
+
+if test ! -e $HOME/miniconda; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+fi
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Install common Python dependencies
+source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
+
+if [[ $SETUP_XVFB == True ]]; then
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+fi
---
+++ setup_conda_osx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Workaround for https://github.com/travis-ci/travis-ci/issues/6307, which
+# caused the following error on MacOS X workers:
+#
+# /Users/travis/build.sh: line 109: shell_session_update: command not found
+#
+rvm get head
+
+# Install conda
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
+
+if test ! -e $HOME/miniconda; then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+fi
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Install common Python dependencies
+source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
