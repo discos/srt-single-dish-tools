509302946e7e248a99697d26265388221d072c91
Matteo Bachetti <matteo@matteobachetti.it>
2017-08-31 11:08:24 +0200

    Increase test coverage; cleanup

--- srttools/core/tests/test_fit.py
+++ srttools/core/tests/test_fit.py
@@ -3,6 +3,9 @@
 from __future__ import (absolute_import, division,
                         print_function)
 from srttools.core.fit import fit_baseline_plus_bell, purge_outliers, align
+from srttools.core.fit import baseline_rough, ref_mad, ref_std, _rolling_window
+from srttools.core.fit import linear_fit, offset_fit
+
 import numpy as np
 import pytest
 
@@ -96,6 +99,32 @@ class TestFit(object):
         np.testing.assert_almost_equal(model.slope_0, 6., 1)
         assert np.abs(model.intercept_0 - 20.) < 2
 
+    def test_fit_baseline_plus_bell_lorentz(self):
+        """Test that the fit procedure works."""
+
+        x = np.arange(0, len(self.series)) * 0.1
+        y = np.copy(self.series) + _test_shape(x) + x * 6 + 20
+
+        model, _ = fit_baseline_plus_bell(x, y, ye=10, kind='lorentz')
+
+        np.testing.assert_almost_equal(model.x_0_1, 50., 1)
+
+    def test_fit_baseline_plus_bell_invalid(self):
+        """Test that the fit procedure works."""
+
+        x = np.arange(0, len(self.series)) * 0.1
+        y = np.copy(self.series) + _test_shape(x) + x * 6 + 20
+        with pytest.raises(ValueError) as excinfo:
+            model, _ = fit_baseline_plus_bell(x, y, ye=10, kind='zxcdf')
+        assert 'kind has to be one of: gauss, lorentz' in str(excinfo)
+
+    def test_fit_baseline_rough_return_baseline(self):
+        """Test that the fit procedure works."""
+
+        x = np.arange(0, len(self.series)) * 0.1
+        y = np.copy(self.series) + _test_shape(x) + x * 6 + 20
+        a, b = baseline_rough(x, y, return_baseline=True)
+
     def test_minimize_align(self):
         """Test that the minimization of the alignment works."""
 
@@ -119,3 +148,35 @@ class TestFit(object):
 
         np.testing.assert_allclose(qs, [-60, 60], atol=3)
         np.testing.assert_allclose(ms, [0.3, -0.8], atol=0.05)
+
+    def test_rolling_window_invalid(self):
+        with pytest.raises(Exception):
+            _rolling_window(1, 5)
+
+    def test_ref_std_small_array(self):
+        array = np.zeros(10)
+        window = 10
+        a = ref_std(array, window)
+        assert a == 0
+
+    def test_ref_mad_small_array(self):
+        array = np.zeros(10)
+        window = 10
+        a = ref_mad(array, window)
+        assert a == 0
+
+    def test_linear_fit_err_not_implemented(self):
+        x = np.arange(10)
+        y = np.zeros(10)
+        with pytest.warns(UserWarning) as record:
+            _, err = linear_fit(x, y, [0, 0], return_err=True)
+            assert "return_err not implemented" in record[0].message.args[0]
+        assert err is None
+
+    def test_offset_fit_err_not_implemented(self):
+        x = np.arange(10)
+        y = np.zeros(10)
+        with pytest.warns(UserWarning) as record:
+            _, err = offset_fit(x, y, 0, return_err=True)
+            assert "return_err not implemented" in record[0].message.args[0]
+        assert err is None
