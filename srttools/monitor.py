c4160e029326bd729d6eba0a2fb6cb30383b04ca
Giuseppe Carboni <giuseppecarboni89@live.com>
2018-11-08 15:06:31 +0100

    Fix #126, fix #127, fix #128, fix #131. Changelog below.
    Fix #126: moved the configuration file reading phase inside `MyHandler` initialization method.
    
    Fix #127, improved the event handling strategy. When the initialization of the `MyHandler` object is completed, a processing thread is started. This threads waits for a file name to be enqueued and starts the processing phase as soon as it receives one. Every time a `created` or `modified` event is raised, the monitor waits for a time threshold of 50 milliseconds. If another event regarding the same file is raised, the monitor keep on waiting another 50 milliseconds, and so on. If no event regarding the same file is received, when 50ms had elapsed, the file gets enqueued for processing. This filtering strategy should be able to discriminate between file system operations and files updated by the backend on the fly (which should take more than 50ms to be updated with new data). Without this strategy, the same file would be enqueued multiple times with no significant difference between the two versions.
    
    Fix #128, now the monitor does not perform an external call to `SDTPreprocess`, it imports the latter's main function and calls it with the same parameters from the processing thread. Since the call gets performed by a non-main thread, `matplotlib.pyplot` was raising some isssues using the `TKAgg` backend. To fix this, the monitor module now imports `pyplot` and set its backend to `Agg` manually. Without this fix, the issue could be propagated to the tests if the check on `DISPLAY` environment variable performed by the `utils` module sets the default matplotlib backend to `TKAgg`.
    
    Fix #131, now the `index.html` page embed a javascript logic to update the images without forcing a page refresh by the browser. The images gets updated with no display flickering, this really helps the observers to pick even a little change in the displayed signal. It has been successfully tested with Mozilla Firefox and Chromium browsers on Linux Ubuntu 18.04 with default configurations.

--- srttools/monitor.py
+++ srttools/monitor.py
@@ -15,63 +15,61 @@ except ImportError:
 import warnings
 import subprocess as sp
 import glob
+from threading import Timer, Thread
+from queue import Queue
 
 from srttools.read_config import read_config
 from srttools.scan import product_path_from_file_name
-global CONFIG_FILE
-
-
-def create_dummy_config():
-    config_str = """
-    [local]
-    [analysis]
-    [debugging]
-    debug_file_format : jpg
-    """
-    with open('monitor_config.ini', 'w') as fobj:
-        print(config_str, file=fobj)
-
-    return 'monitor_config.ini'
+from srttools.imager import main_preprocess
+import matplotlib.pyplot as plt
+plt.switch_backend('Agg')
 
 
 class MyHandler(PatternMatchingEventHandler):
     patterns = ["*/*.fits"]
 
-    def __init__(self, nosave=False):
-        self.nosave = nosave
+    def __init__(self, config_file, nosave=False):
         super().__init__()
+        self.config_file = config_file
+        self.conf = read_config(self.config_file)
+        self.nosave = nosave
+        self.ext = self.conf['debug_file_format']
+        create_index_file(self.ext)
+        self.filequeue = Queue()
+        self.timers = {}
+        t = Thread(target=self._dequeue)
+        t.daemon = True
+        t.start()
 
-    def process(self, event):
-        """
-        event.event_type
-            'modified' | 'created' | 'moved' | 'deleted'
-        event.is_directory
-            True | False
-        event.src_path
-            path/to/observed/file
-        """
-        global CONFIG_FILE
+    def _enqueue(self, infile):
+        if self.timers.get(infile):
+            del self.timers[infile]
+        if infile not in self.filequeue.queue:
+            self.filequeue.put(infile)
 
-        infile = event.src_path
-        conf = read_config(CONFIG_FILE)
-        ext = conf['debug_file_format']
-        productdir, fname = \
-            product_path_from_file_name(infile, productdir=conf['productdir'],
-                                        workdir=conf['workdir'])
+    def _dequeue(self):
+        while True:
+            self._process(self.filequeue.get())
 
+    def _process(self, infile):
+        productdir, fname = product_path_from_file_name(
+            infile,
+            productdir=self.conf['productdir'],
+            workdir=self.conf['workdir']
+        )
         root = os.path.join(productdir, fname.replace('.fits', ''))
 
+        pp_args = ['--debug', '-c', self.config_file]
+        if self.nosave:
+            pp_args.append('--nosave')
+        pp_args.append(infile)
         try:
-            cmd_string = "SDTpreprocess --debug {} "
-            if self.nosave:
-                cmd_string += "--nosave "
-            cmd_string += "-c {}"
-            sp.check_call(cmd_string.format(infile, CONFIG_FILE).split())
-        except sp.CalledProcessError:
+            main_preprocess(pp_args)
+        except:
             return
 
         newfiles = []
-        for debugfile in glob.glob(root + '*.{}'.format(ext)):
+        for debugfile in glob.glob(root + '*.{}'.format(self.ext)):
             newfile = debugfile.replace(root, 'latest')
             newfiles.append(newfile)
             cmd_string = ''
@@ -80,42 +78,35 @@ class MyHandler(PatternMatchingEventHandler):
             else:
                 cmd_string = 'cp {} {}'
             sp.check_call(cmd_string.format(debugfile, newfile).split())
-        if self.nosave and conf['productdir'] \
-                and conf['workdir'] not in conf['productdir']:
-            prodpath = os.path.relpath(root, conf['productdir'])
+        if self.nosave and self.conf['productdir'] \
+                and self.conf['workdir'] not in self.conf['productdir']:
+            prodpath = os.path.relpath(root, self.conf['productdir'])
             prodpath = prodpath.split('/')[0]
-            prodpath = os.path.join(conf['productdir'], prodpath)
+            prodpath = os.path.join(self.conf['productdir'], prodpath)
             if os.path.exists(prodpath):
                 shutil.rmtree(prodpath)
 
-        oldfiles = glob.glob('latest*.{}'.format(ext))
-        with open('index.html', "w") as fobj:
-            print('<META HTTP-EQUIV="refresh" CONTENT="5">', file=fobj)
-            N = len(newfiles)
-            if N <= 2:
-                width = "50%"
-            else:
-                width = "25%"
-            for fname in sorted(newfiles):
-                print("<div style=\"width:{}; float:left;\" />".format(width),
-                      file=fobj)
-                print("<img src=\"{}\" width=\"100%\"/>".format(fname),
-                      file=fobj)
-                print("</div>", file=fobj)
+        oldfiles = glob.glob('latest*.{}'.format(self.ext))
         for oldfile in oldfiles:
             if oldfile not in newfiles:
                 os.remove(oldfile)
 
-    def on_created(self, event):
-        self.process(event)
-
     def on_modified(self, event):
-        self.process(event)
+        self._start_timer(event.src_path)
+
+    def on_created(self, event):
+        self._start_timer(event.src_path)
+
+    def _start_timer(self, infile):
+        if self.timers.get(infile):
+            self.timers[infile].cancel()
+
+        self.timers[infile] = Timer(0.05, self._enqueue, args=[infile])
+        self.timers[infile].start()
 
 
 def main_monitor(args=None):
     import argparse
-    global CONFIG_FILE
 
     description = ('Run the SRT quicklook in a given directory.')
     parser = argparse.ArgumentParser(description=description)
@@ -160,16 +151,12 @@ def main_monitor(args=None):
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    with open('index.html', "w") as fobj:
-        print('<META HTTP-EQUIV="refresh" CONTENT="5">', file=fobj)
-        print("Waiting for the first observation...", file=fobj)
-
     if args.config is None:
-        CONFIG_FILE = create_dummy_config()
+        config_file = create_dummy_config()
     else:
-        CONFIG_FILE = args.config
+        config_file = args.config
 
-    event_handler = MyHandler(nosave=args.nosave)
+    event_handler = MyHandler(config_file, nosave=args.nosave)
     observer = None
     if args.polling:
         observer = PollingObserver()
@@ -190,3 +177,93 @@ def main_monitor(args=None):
     except KeyboardInterrupt:
         pass
     observer.stop()
+
+
+def create_dummy_config():
+    config_str = """
+    [local]
+    [analysis]
+    [debugging]
+    debug_file_format : jpg
+    """
+    with open('monitor_config.ini', 'w') as fobj:
+        print(config_str, file=fobj)
+
+    return 'monitor_config.ini'
+
+
+def create_index_file(extension, max_images=50, interval=500):
+    """
+    :param extension: the file extension of the image files to look for.
+    :param max_images: the maximum number of images to monitor. It should be
+        enough to set it to twice the number of feeds of the receiver having
+        the highest number of feeds (twice because of L and R channels).
+        Its default value is set to 50, a number high enough to account for all
+        the file images but not too high to represent a computational
+        overhead. Since javascript cannot perform any client filesystem
+        operation other than loading a local file from its path, the script
+        below tries to access every image and just hides the not found ones,
+        displaying only the images coming out of the monitor processing phase.
+    :param interval: expressed in milliseconds, it represents the time between
+        two subsequent calls to the `updatePage` function. Since the images
+        get reloaded without any flickering (as opposed to when the whole page
+        gets reloaded from the browser), its value can be set to a fraction of
+        a second without any visible issue.
+    """
+    html_string = \
+    """<html>
+    <script type="text/javascript">
+        window.onload = function()
+        {
+            var extension = '""" + extension + """';
+            var maxImages = """ + str(max_images) + """;
+			var interval = """ + str(interval) + """;
+
+            document.body.innerHTML = "";
+
+            for(i = 0; i < maxImages; i++)
+            {
+                document.body.innerHTML += '<div id="div_' + i + '" style="width:50%; float:left;"/></div>';
+            }
+
+            function updatePage()
+            {
+                for(i = 0; i < maxImages; i++)
+                {
+                    image_id = "img_" + i;
+
+                    var image = document.getElementById(image_id);
+
+                    if(image == null)
+                    {
+                        image = new Image();
+                        image.id = image_id;
+                        image.style.width = "100%";
+
+                        image.addEventListener("load", function()
+                        {
+                            if(this.parentElement == null)
+                            {
+                                var div = document.getElementById("div_" + this.id.split("_")[1]);
+                                div.appendChild(this);
+                            }
+                        });
+
+                        image.addEventListener("error", function()
+                        {
+                            var div = document.getElementById("div_" + this.id.split("_")[1]);
+                            div.innerHTML = "";
+                        });
+                    }
+
+                    image.src = "latest_" + i + "." + extension + "?" + new Date().getTime();
+                }
+            }
+
+            updatePage();
+            setInterval(updatePage, interval);
+        }
+    </script>\n</html>"""
+
+    with open('index.html', 'w') as fobj:
+        print(html_string, file=fobj)
