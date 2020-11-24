c42e24b61d582a88a4eae26c2419f882caa1c934
Matteo Bachetti <matteo@matteobachetti.it>
2020-11-24 13:04:45 +0100

    Update infrastructure: Github Actions and tox for tests

--- srttools/__init__.py
+++ srttools/__init__.py
@@ -1,13 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""
-This is an Astropy affiliated package.
-"""
-
-# Affiliated packages may add whatever they like to this file, but
+# Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *  # NOQA
+from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 try:
     import faulthandler
@@ -16,31 +12,4 @@ try:
 except ImportError:
     pass
 
-# For egg_info test builds to pass, put package imports here.
-if not _ASTROPY_SETUP_:
-    import sys
-    import warnings
-    from astropy import log
-
-    warnings.simplefilter("ignore")
-    warnings.filterwarnings("once", module="srttools", category=UserWarning)
-    warnings.filterwarnings("once", module="srttools",
-                            category=DeprecationWarning)
-    if sys.version_info[0] < 3:
-        warnings.warn("You are using the srttools under Python 2. "
-                      "Limited support for Python 2 will only be "
-                      "maintained until the end of 2018. Please update "
-                      "your Python setup from 2 to 3.5 or later.",
-                      DeprecationWarning)
-    # from .calibration import *  # noqa: F401,F403
-    # from .fit import *  # noqa: F401,F403
-    # from .global_fit import *  # noqa: F401,F403
-    # from .histograms import *  # noqa: F401,F403
-    # from .imager import *  # noqa: F401,F403
-    # from .inspect_observations import *  # noqa: F401,F403
-    # from .interactive_filter import *  # noqa: F401,F403
-    # from .io import *  # noqa: F401,F403
-    # from .read_config import *  # noqa: F401,F403
-    # from .scan import *  # noqa: F401,F403
-    # from .simulate import *  # noqa: F401,F403
-    # from .utils import *  # noqa: F401,F403
+__all__ = []
