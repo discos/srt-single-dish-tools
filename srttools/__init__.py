809c2e180dd49e9ae17102ecd194ea2a3077b9f6
Matteo Bachetti <matteo@matteobachetti.it>
2021-03-30 22:35:51 +0200

    Fix annoying legend error

--- srttools/__init__.py
+++ srttools/__init__.py
@@ -15,6 +15,7 @@ except ImportError:
 import warnings
 import numpy as np
 # warnings.filterwarnings("error", category=np.VisibleDeprecationWarning)
+# warnings.filterwarnings("error", ".*")
 warnings.filterwarnings("once", category=UserWarning)
 warnings.filterwarnings("once", category=DeprecationWarning)
 warnings.filterwarnings("once", category=np.VisibleDeprecationWarning)
