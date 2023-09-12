3fcd13efd715949f26237fa8f16e32b203eb84c6
Matteo Bachetti <matteo@matteobachetti.it>
2023-09-12 11:49:13 +0200

    Exclude changes/ directory from docs build [docs only]

--- docs/conf.py
+++ docs/conf.py
@@ -33,26 +33,27 @@ from importlib import import_module
 try:
     from sphinx_astropy.conf.v1 import *  # noqa
 except ImportError:
-    print('ERROR: the documentation requires the sphinx-astropy package to be installed')
+    print("ERROR: the documentation requires the sphinx-astropy package to be installed")
     sys.exit(1)
 
-ON_RTD = os.environ.get('READTHEDOCS') == 'True'
-ON_TRAVIS = os.environ.get('TRAVIS') == 'true'
+ON_RTD = os.environ.get("READTHEDOCS") == "True"
+ON_TRAVIS = os.environ.get("TRAVIS") == "true"
 
 # Get configuration information from setup.cfg
 from configparser import ConfigParser
+
 conf = ConfigParser()
 
-conf.read([os.path.join(os.path.dirname(__file__), '..', 'setup.cfg')])
-setup_cfg = dict(conf.items('metadata'))
+conf.read([os.path.join(os.path.dirname(__file__), "..", "setup.cfg")])
+setup_cfg = dict(conf.items("metadata"))
 
 # -- General configuration ----------------------------------------------------
 
 # By default, highlight as Python 3.
-highlight_language = 'python3'
+highlight_language = "python3"
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.2'
+# needs_sphinx = '1.2'
 
 # To perform a Sphinx version check that needs to be more specific than
 # major.minor, call `check_sphinx_version("X.Y.Z")` here.
@@ -60,7 +61,8 @@ highlight_language = 'python3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns.append('_templates')
+exclude_patterns.append("_templates")
+exclude_patterns.append("changes")
 
 # This is added to the end of RST files - a good place to put substitutions to
 # be used globally.
@@ -70,20 +72,19 @@ rst_epilog += """
 # -- Project information ------------------------------------------------------
 
 # This does not *have* to match the package name, but typically does
-project = setup_cfg['name']
-author = setup_cfg['author']
-copyright = '{0}, {1}'.format(
-    datetime.datetime.now().year, setup_cfg['author'])
+project = setup_cfg["name"]
+author = setup_cfg["author"]
+copyright = "{0}, {1}".format(datetime.datetime.now().year, setup_cfg["author"])
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-import_module(setup_cfg['name'])
-package = sys.modules[setup_cfg['name']]
+import_module(setup_cfg["name"])
+package = sys.modules[setup_cfg["name"]]
 
 # The short X.Y version.
-version = package.__version__.split('-', 1)[0]
+version = package.__version__.split("-", 1)[0]
 # The full version, including alpha/beta/rc tags.
 release = package.__version__
 
@@ -100,42 +101,42 @@ release = package.__version__
 
 # Add any paths that contain custom themes here, relative to this directory.
 # To use a different custom theme, add the directory containing the theme.
-html_theme_path = ['themes']
+html_theme_path = ["themes"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes. To override the custom theme, set this to the
 # name of a builtin theme or the name of a custom theme in html_theme_path.
-#html_theme = None
-html_theme = 'bootstrap-astropy'
+# html_theme = None
+html_theme = "bootstrap-astropy"
 
 html_theme_options = {
-    'logotext1': 'SRT',  # white,  semi-bold
-    'logotext2': '-SDT',  # orange, light
-    'logotext3': ':docs'   # white,  light
-    }
+    "logotext1": "SRT",  # white,  semi-bold
+    "logotext2": "-SDT",  # orange, light
+    "logotext3": ":docs",  # white,  light
+}
 
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+# html_sidebars = {}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/srttools.png'
+html_logo = "images/srttools.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'images/srttools.ico'
+html_favicon = "images/srttools.ico"
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%Y-%m-%d %H:%M:%S'
+html_last_updated_fmt = "%Y-%m-%d %H:%M:%S"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = '{0} v{1}'.format(project, release)
+html_title = "{0} v{1}".format(project, release)
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = project + 'doc'
+htmlhelp_basename = project + "doc"
 
 # Prefixes that are ignored for sorting the Python module index
 modindex_common_prefix = ["srttools."]
@@ -145,64 +146,61 @@ modindex_common_prefix = ["srttools."]
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
-latex_documents = [('index', project + '.tex', project + u' Documentation',
-                    author, 'manual')]
+latex_documents = [("index", project + ".tex", project + " Documentation", author, "manual")]
 
 
 # -- Options for manual page output -------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [('index', project.lower(), project + u' Documentation',
-              [author], 1)]
+man_pages = [("index", project.lower(), project + " Documentation", [author], 1)]
 
 
 # -- Options for the edit_on_github extension ---------------------------------
 
-if setup_cfg.get('edit_on_github').lower() == 'true':
+if setup_cfg.get("edit_on_github").lower() == "true":
+    extensions += ["sphinx_astropy.ext.edit_on_github"]
 
-    extensions += ['sphinx_astropy.ext.edit_on_github']
-
-    edit_on_github_project = setup_cfg['github_project']
+    edit_on_github_project = setup_cfg["github_project"]
     edit_on_github_branch = "main"
 
     edit_on_github_source_root = ""
     edit_on_github_doc_root = "docs"
 
 # -- Resolving issue number to links in changelog -----------------------------
-github_issues_url = 'https://github.com/{0}/issues/'.format(setup_cfg['github_project'])
+github_issues_url = "https://github.com/{0}/issues/".format(setup_cfg["github_project"])
 
 if not ON_RTD and not ON_TRAVIS:
-    scripts = dict(conf.items('options.entry_points'))['console_scripts'].split("\n")
+    scripts = dict(conf.items("options.entry_points"))["console_scripts"].split("\n")
     import subprocess as sp
-    with open(os.path.join(os.getcwd(), 'scripts',
-                           'cli.rst'), 'w') as fobj:
+
+    with open(os.path.join(os.getcwd(), "scripts", "cli.rst"), "w") as fobj:
         print("""Command line interface""", file=fobj)
         print("""======================\n""", file=fobj)
 
         for script in sorted(scripts):
             try:
-                cl, fun = [val.strip() for val in script.split('=')]
+                cl, fun = [val.strip() for val in script.split("=")]
             except ValueError:
                 continue
             print(cl.strip(), file=fobj)
-            print('-' * len(cl), file=fobj)
+            print("-" * len(cl), file=fobj)
             print(file=fobj)
-            print('.. code-block:: none', file=fobj)
+            print(".. code-block:: none", file=fobj)
             print(file=fobj)
-            lines = sp.check_output([cl, '--help']).decode().split('\n')
+            lines = sp.check_output([cl, "--help"]).decode().split("\n")
             for l in lines:
-                if l.strip() == '':
+                if l.strip() == "":
                     print(file=fobj)
                 else:
-                    print('    ' + l, file=fobj)
+                    print("    " + l, file=fobj)
             print(file=fobj)
 
 
 # -- Options for linkcheck output -------------------------------------------
 linkcheck_retry = 5
 linkcheck_ignore = [
-    r'https://github\.com/discos/srt-single-dish-tools/(?:issues|pull)/\d+',
+    r"https://github\.com/discos/srt-single-dish-tools/(?:issues|pull)/\d+",
 ]
 linkcheck_timeout = 180
 linkcheck_anchors = False
@@ -210,8 +208,7 @@ linkcheck_anchors = False
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
 #
 nitpicky = True
-nitpick_ignore = [('py:obj', 'histogram'),
-                  ('py:func', 'histogram')]
+nitpick_ignore = [("py:obj", "histogram"), ("py:func", "histogram")]
 #
 # Some warnings are impossible to suppress, and you can list specific references
 # that should be ignored in a nitpick-exceptions file which should be inside
